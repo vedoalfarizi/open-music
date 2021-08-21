aac93f19e42860ca561021f1287a8a87cab41eff
telkomdev-vedo <vedoalfarizi@gmail.com>
2021-08-21 23:45:40 +0700

    bugfix: save song validation

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -4,10 +4,10 @@ const { ONE_HOUR } = require('../../utils/constans');
 
 const songPayloadSchema = joi.object({
   title: joi.string().max(255).required(),
-  year: joi.number().min(1000),
-  performer: joi.string().max(64),
-  genre: joi.string().max(16),
-  duration: joi.number().min(10).max(13 * ONE_HOUR),
+  year: joi.number().min(1000).required(),
+  performer: joi.string().max(64).required(),
+  genre: joi.string().max(16).required(),
+  duration: joi.number().min(10).max(13 * ONE_HOUR).required(),
 });
 
 module.exports = {
