5c8ae58f99cc5fc133a576bbac4b2a44810be4fc
telkomdev-vedo <vedoalfarizi@gmail.com>
2021-08-21 23:46:27 +0700

    refactor response

--- src/utils/wrapper.js
+++ src/utils/wrapper.js
@@ -1,24 +1,37 @@
 const logger = require('./logger');
 
-const sendResponse = (h, status, result, message = '', code = 200) => {
-  if (status === 'fail') {
-    const res = h.response({
-      status,
-      message: result.statusCode ? result.message : 'Maaf, terjadi kesalahan di sistem kami',
-    });
-    res.code(result.statusCode || 500);
+const successResponse = (h, data, message = null, code = 200) => {
+  const payload = {
+    status: 'success',
+    data,
+  };
 
-    if (!result.statusCode) logger('send-response', result.message, 'error');
+  if (message) payload.message = message;
+
+  const res = h.response(payload);
+  res.code(code);
+
+  return res;
+};
+
+const errorResponse = (h, response) => {
+  if (!response.statusCode) { // Server Error
+    const res = h.response({
+      status: 'fail',
+      message: 'Maaf, terjadi kesalahan pada sistem kami',
+    });
+    res.code(500);
+
+    logger('wrapper-errorResponse', response.message, 'error');
 
     return res;
   }
 
   const res = h.response({
-    status,
-    data: result,
-    message,
+    status: 'fail',
+    message: response.message,
   });
-  res.code(code);
+  res.code(response.statusCode);
 
   return res;
 };
@@ -27,13 +40,13 @@ const preResponse = (request, h) => {
   const { response } = request;
 
   if (response instanceof Error) {
-    return sendResponse(h, 'fail', response);
+    return errorResponse(h, response);
   }
 
   return response.continue || response;
 };
 
 module.exports = {
-  sendResponse,
+  successResponse,
   preResponse,
 };
